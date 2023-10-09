a911eeae23692c8b92be689e1af9147f68c639c8
Hussain Ben Alshaikh <nimo1423@gmail.com>
2023-10-09 17:08:25 +0100

    exercise 3 4th try

--- things/models.py
+++ things/models.py
@@ -3,6 +3,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.core.exceptions import ValidationError
 
 class Thing(models.Model):
-    name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.CharField(max_length=120, blank=True)
-    quantity= models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=120)
+    quantity= models.PositiveIntegerField()
