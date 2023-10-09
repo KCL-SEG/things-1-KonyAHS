e53887cda33871e70c43d651a7e8968941bddd39
Hussain Ben Alshaikh <nimo1423@gmail.com>
2023-10-09 17:14:23 +0100

    exercise 3 6th try fix 0 inclusion

--- things/models.py
+++ things/models.py
@@ -5,4 +5,4 @@ from django.core.exceptions import ValidationError
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(max_length=120, unique=False, blank=True)
-    quantity= models.PositiveIntegerField(unique=False, validators=[MinValueValidator(1), MaxValueValidator(100)])
+    quantity= models.PositiveIntegerField(unique=False, validators=[MinValueValidator(0), MaxValueValidator(100)])
